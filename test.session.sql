bdb4572323eb7486d22ebff95fd9d4f3c925cfe5
Mimi <trish21215@yahoo.com>
2025-06-16 22:45:22 -0400

    finished exercise 2

--- test.session.sql
+++ test.session.sql
@@ -1,9 +1,19 @@
-CREATE TABLE books (
-    book_id PRIMARY KEY,
-    title TEXT NOT NULL,
-    author TEXT,
-    published_year DATE,
-    is_available INT(1)
-);
+-- CREATE TABLE books (
+--     book_id INT PRIMARY KEY,
+--     title TEXT NOT NULL,
+--     author TEXT,
+--     published_year DATE,
+--     is_available INT(1)
+-- );
 
 -- DROP TABLE books;
+
+INSERT INTO books (title, author, published_year, is_available)
+VALUES
+('The Great Gatsby', 'F. Scott Fitzgerald', 1925, 1),
+('To Kill a Mockingbird', 'Harper Lee', 1960, 1),
+('1984', 'George Orwell', 1949, 0);
+
+SELECT * FROM books;
+SELECT * FROM books WHERE is_available = 1;
+SELECT * FROM books WHERE published_year < 1950;
